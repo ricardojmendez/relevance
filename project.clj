4b26cf9cfba5f9c146907e803d73e22b4ee07e60
Ricardo J. Mendez <ricardo@numergent.com>
2015-09-16 15:58:02 +0300

    Initial commit from template

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject tabler "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [khroma "0.0.2"]
+                 [prismatic/dommy "0.1.2"]]
+  :source-paths ["src"]
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.3"]
+                             [lein-cljsbuild "1.0.1"]
+                             [lein-chromebuild "0.2.1"]]
+
+                   :cljsbuild {:builds {:main
+                                        {:source-paths ["src"]
+                                         :compiler {:output-to "target/unpacked/tabler.js"
+                                                    :output-dir "target/js"
+                                                    :optimizations :whitespace
+                                                    :pretty-print true}}}}
+                   }}) 
---
+++ resources/js/background.js
@@ -0,0 +1,1 @@
+tabler.background.init();
