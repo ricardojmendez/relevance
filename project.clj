720cc741923e210efaa892c5019ffaee8d98852d
Ricardo J. Mendez <ricardo@numergent.com>
2015-09-16 16:43:02 +0300

    Updating dependencies
    Removed austin plugin as it was failing to compile with the lastest
    ClojureScript, will need to replace with a different browser repl

--- project.clj
+++ project.clj
@@ -1,20 +1,19 @@
 (defproject tabler "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [khroma "0.0.2"]
-                 [prismatic/dommy "0.1.2"]]
+                 [prismatic/dommy "1.1.0"]]
   :source-paths ["src"]
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.3"]
-                             [lein-cljsbuild "1.0.1"]
-                             [lein-chromebuild "0.2.1"]]
+  :profiles {:dev {:plugins   [[lein-cljsbuild "1.1.0"]
+                               [lein-chromebuild "0.2.1"]]
 
                    :cljsbuild {:builds {:main
                                         {:source-paths ["src"]
-                                         :compiler {:output-to "target/unpacked/tabler.js"
-                                                    :output-dir "target/js"
-                                                    :optimizations :whitespace
-                                                    :pretty-print true}}}}
+                                         :compiler     {:output-to     "target/unpacked/tabler.js"
+                                                        :output-dir    "target/js"
+                                                        :optimizations :whitespace
+                                                        :pretty-print  true}}}}
                    }}) 
