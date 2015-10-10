ricardojmendez/relevance

commit d7ff2ac98b8197dbbb751a1694105f88b3f16343

No need to save the tab id on either groups or snapshots, it's irrelevant

src/booklet/core.cljs
@@ -61,6 +61,8 @@
   [col id]
   (remove #(= (:id %) id) col))
 
+;; Tab items we actually care about
+(def relevant-tab-items [:index :url :title :favIconUrl])
 
 
 ;;;;----------------------------
@@ -104,7 +106,7 @@
   :group-add
   (fn [app-state [_]]
     (let [tabs    (get-in app-state [:app-state :tabs])
-          to-save (map (fn [m] (select-keys m [:index :url :id :title :favIconUrl])) tabs)
+          to-save (map (fn [m] (select-keys m relevant-tab-items)) tabs)
           groups  (conj (or (get-in app-state [:data :groups]) '())
                         (group-from-tabs to-save))]
       (dispatch [:data-set :groups groups])
@@ -190,7 +192,7 @@
           snapshots (or (get-in app-state path) '())
           tabs      (->> (get-in app-state [:app-state :tabs])
                          filter-tabs
-                         (map #(select-keys % [:index :url :id :title :favIconUrl])))
+                         (map #(select-keys % relevant-tab-items)))
           new-group (group-from-tabs tabs)
           last-snap (or (get-in app-state [:app-state :last-snapshot])
                         (first (sort-by #(* -1 (:date %)) snapshots)))

commit 41c927da3454303fed4bf3ccb5ef6422b2c9d7c8

Fixed minor regression from commit d7ff2ac
I was using the tab id as the key for react, so after removing it I
started getting warnings about needing unique indexes. Using map-indexed
solves the problem.

src/booklet/core.cljs
@@ -362,24 +362,28 @@
         ]])))
 
 (defn list-tabs [tabs is-history?]
-  (for [tab (sort-by :index tabs)]
-    (let [url     (:url tab)
-          favicon (:favIconUrl tab)
-          action  (if is-history?
-                    {:href url :target "_blank"}
-                    {:on-click #(tabs/activate (:id tab))}
-                    )]
-      ^{:key (:id tab)}
-      [:tr
-       [:td {:class "col-sm-1"} (if-not is-history? (:id tab))]
-       [:td {:class "col-sm-6"} [:a
-                                 action
-                                 (if favicon
-                                   [:img {:src    favicon
-                                          :width  16
-                                          :height 16}])
-                                 (:title tab)]]
-       [:td {:class "col-sm-5"} url]])))
+  (->>
+    tabs
+    (sort-by :index)
+    (map-indexed
+      (fn [i tab]
+        (let [url     (:url tab)
+              favicon (:favIconUrl tab)
+              action  (if is-history?
+                        {:href url :target "_blank"}
+                        {:on-click #(tabs/activate (:id tab))}
+                        )]
+          ^{:key i}
+          [:tr
+           [:td {:class "col-sm-1"} (if-not is-history? (:id tab))]
+           [:td {:class "col-sm-6"} [:a
+                                     action
+                                     (if favicon
+                                       [:img {:src    favicon
+                                              :width  16
+                                              :height 16}])
+                                     (:title tab)]]
+           [:td {:class "col-sm-5"} url]])))))
 
 (defn current-tabs []
   (let [tabs (reaction (filter-tabs @(subscribe [:app-state :tabs])))]