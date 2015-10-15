36a65d19cbba6c80c433ad9fe5da3a0dea61234b
Ricardo J. Mendez <ricardo@numergent.com>
2015-10-15 10:33:09 +0300

    Bumping snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject booklet-chrome "0.1.0"
+(defproject booklet-chrome "0.2.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
