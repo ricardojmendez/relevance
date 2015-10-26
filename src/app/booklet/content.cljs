308acccc5a1e6e8bbdf06798ab731ec12a0e5066
Ricardo J. Mendez <ricardo@numergent.com>
2015-10-26 19:46:13 +0200

    Re-ordering start page results based on time spent on the page

--- src/app/booklet/content.cljs
+++ src/app/booklet/content.cljs
@@ -8,35 +8,45 @@
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 
-(defn transform-node [database node]
-  (let [parent (.-parentNode node)
-        href   (.-href parent)
-        id     (.hashCode href)
-        data   (get database id)]
+(defn transform-result-node!
+  [database node]
+  (let [parent    (.-parentNode node)
+        href      (.-href parent)
+        id        (.hashCode href)
+        data      (get database id)
+        time      (:time data)
+        root-item (-> parent .-parentNode .-parentNode .-parentNode)  ; Yeah, hacky as fuck
+        ]
+    (aset node "rootItem" root-item)
+    (aset root-item "total-time" time)
     (when data
       (aset node "textContent"
-            (str (aget node "textContent") " [time viewed: " (:time data) " ms]"))
-      )
-    )
-  )
+            (str (aget node "textContent") " [time viewed: " time " ms]")))))
 
-(defn do-transformations []
+(defn do-transformations! []
   (go
-    (let [data (from-transit (:data (<! (storage/get))))]
-      (doseq [node (sel :.result_url_heading)]
-        (transform-node (:url-times data) node)
-        #_(aset node "textContent"
-                (str
-                  (aget node "textContent")
-                  " - Hi there: "
-                  (.-href (.-parentNode node)))))))
-  (console/log (sel :.result_url_heading))
-  (console/log (map dommy/text (sel :.result_url_heading)))
-  (console/log (map #(.-parentNode %) (sel :.result_url_heading))))
+    (let [data  (from-transit (:data (<! (storage/get))))
+          nodes (sel :.result_url_heading)
+          base  (sel1 :.web_regular_results)]
+      (doseq [node nodes]
+        (transform-result-node! (:url-times data) node))
+      (console/log "Base" base)
+      (doall
+        (->>
+          nodes
+          (map #(aget % "rootItem"))
+          (map-indexed #(do
+                         ;; We assign it (- 100 %1) so that the first nodes get a higher value
+                         (aset %2 "sort-order" (or (aget %2 "total-time") (- 100 %1)))
+                         %2))
+          (sort-by #(* -1 (aget % "sort-order")))
+          (map #(.appendChild base %))))
+
+      )))
 
 (defn ^:export init []
   (console/log "Init on content script!")
-  (do-transformations)
+  (do-transformations!)
   (let [bg (runtime/connect)]
     (go
       (>! bg :content-initialized)
