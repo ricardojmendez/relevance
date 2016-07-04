41ff415d9cf934b6c3fc7c05b11d3992defb88de
Ricardo J. Mendez <ricardo@numergent.com>
2016-07-04 08:08:38 +0300

    Bumping project version to 1.0.7-SNAPSHOT
    Keeping Manifest version the same, Chrome doesn't like characters on the
    version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject relevance-chrome "1.0.6"
+(defproject relevance-chrome "1.0.7-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
