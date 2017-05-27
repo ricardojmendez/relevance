8f369296aa017ad59feea624140e87ad9e209e74
Ricardo J. Mendez <ricardo@numergent.com>
2017-05-27 09:37:37 +0200

    StartPage integration is now optional

--- src/content/relevance/startpage.cljs
+++ src/content/relevance/startpage.cljs
@@ -1,5 +1,6 @@
 (ns relevance.startpage
   (:require [relevance.io :as io]
+            [relevance.settings :as settings]
             [relevance.utils :refer [url-key time-display]]
             [dommy.core :refer-macros [sel sel1] :as dommy]
             [khroma.runtime :as runtime]
@@ -20,7 +21,7 @@
         id        (url-key href)
         data      (get database id)
         time      (:time data)
-        root-item (-> parent .-parentNode .-parentNode .-parentNode)  ; Yeah, hacky as fuck
+        root-item (-> parent .-parentNode .-parentNode .-parentNode) ; Yeah, hacky as fuck
         ]
     (aset node "rootItem" root-item)
     (aset root-item "total-time" time)
@@ -41,23 +42,25 @@
 
 (defn do-transformations! []
   (go
-    (let [data  (<! (io/load :data))
-          nodes (sel :.result_url_heading)
-          base  (sel1 :.web_regular_results)]
-      (doseq [node nodes]
-        (transform-result-node! (:url-times data) node))
-      (doall
-        (->>
-          nodes
-          (map #(aget % "rootItem"))
-          (map-indexed #(do
-                         ;; We assign it (- %1) so that the first nodes get a higher value
-                         (aset %2 "sort-order" (or (aget %2 "total-time") (- %1)))
-                         %2))
-          (sort-by #(* -1 (aget % "sort-order")))
-          (map #(.appendChild base %))))
+    (when (:startpage? (or (<! (io/load :settings))
+                           settings/default-settings))
+      (let [data  (<! (io/load :data))
+            nodes (sel :.result_url_heading)
+            base  (sel1 :.web_regular_results)]
+        (doseq [node nodes]
+          (transform-result-node! (:url-times data) node))
+        (doall
+          (->>
+            nodes
+            (map #(aget % "rootItem"))
+            (map-indexed #(do
+                            ;; We assign it (- %1) so that the first nodes get a higher value
+                            (aset %2 "sort-order" (or (aget %2 "total-time") (- %1)))
+                            %2))
+            (sort-by #(* -1 (aget % "sort-order")))
+            (map #(.appendChild base %))))
 
-      )))
+        ))))
 
 (defn ^:export main []
   ; (console/log "Init on content script!")
@@ -65,7 +68,7 @@
   (let [bg (runtime/connect)]
     (go
       (>! bg :content-initialized)
-      #_ (console/log "<-- Background replied" (<! bg))
+      #_(console/log "<-- Background replied" (<! bg))
       )))
 
 (main)
