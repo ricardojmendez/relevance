2edf83d8010fa458961f1deae38f64e5106ec62e
Ricardo J. Mendez <ricardo@numergent.com>
2017-11-04 10:17:13 +0100

    Bumping development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject relevance-chrome "1.1.0"
+(defproject relevance-chrome "1.1.1-SNAPSHOT"
   :license {:name "MIT License"
             :url  "https://tldrlegal.com/license/mit-license"}
   :dependencies [[org.clojure/clojure "1.8.0"]
